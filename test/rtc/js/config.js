b0d772b69427cd5eb615c4ab180b4fdf68254617
赵磊 <lei40251@163.com>
2020-06-05 20:39:25 +0800

    清理rtc文件夹

---
+++ test/rtc/js/config.js
@@ -0,0 +1,8 @@
+var domain = 'lccsp.zgpajf.com.cn';
+var wss = 'wss://lccsp.zgpajf.com.cn:5092/wss';
+
+// var domain = 'pro.vsbc.com';
+// var wss = 'wss://pro.vsbc.com:60040/wss';
+
+// var domain = 'rtc.vsbc.com';
+// var wss = 'wss://rtc.vsbc.com:5092/wss';
